da75f2ba633a36cd6697746e2f4a49a25e1ec8db
Rebekah Kirkman <rebekah3@email.unc.edu>
2023-01-29 12:19:18 -0500

    finished_ex01_chardle.py

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,44 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730401129"
+
+word: str = input("Enter a five character word: ")
+
+if  len(word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+else:
+    character: str = input("Enter a single character: ")
+
+if len(character) != 1:
+    print("Error: Character must be a single character.")
+    exit()
+else:
+    print("Searching for " + character + " in " + word)
+
+number: int = 0
+
+if (character == word[0]):
+    print(character + " found at index 0")
+    number += 1
+if (character == word[1]):
+    print(character + " found at index 1")
+    number += 1
+if (character == word[2]):
+    print(character + " found at index 2")
+    number += 1
+if (character == word[3]):
+    print(character + " found at index 3")
+    number += 1
+if (character == word[4]):
+    print(character + " found at index 4")
+    number += 1
+
+number_1 = str(number)
+
+if number_1 == "0":
+    print ("No instances of " + character + " found in " + word)
+else:
+    if number_1 == "1":
+        print("1 instance of " + character + " found in " + word)
+    else:
+        print(number_1 + " instances of " + character + " found in " + word)
---
+++ exercises/lessons/conditionals.py
@@ -0,0 +1,14 @@
+"""checks, light, if green, says go"""
+light: str = input("what color is the light?")
+
+if (light == "green"):
+    print("go")
+else: 
+    if (light != "red"):
+        
+    if (light == "yellow"):
+        print ("slow down")
+    else:
+        print("go report this to the authorities.")
+    print("stop")
+print("don't look at phone")
